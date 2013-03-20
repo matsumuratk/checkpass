a8c11fe1a21eaf4937c4929a1c0814e3b0e6ccd1
Taku Matsumura <taku@MARINOS.(none)>
2013-03-20 23:58:06 +0900

    update

--- config/initializers/checkpass.rb
+++ config/initializers/checkpass.rb
@@ -3,7 +3,7 @@
 module PAYPAL
 	CONFIG = YAML.load_file( "#{Rails.root}/config/checkpass.yml" )[Rails.env]
   CHECKOUT_URL = CONFIG['paypal_checkout_url']
-  CANCEL_URL = CONFIG['paypal_cancelurl']
+  CANCEL_URL = CONFIG['paypal_cancel_url']
   NOTIFY_URL = CONFIG['paypal_notify_url']
   ID = CONFIG['paypal_id']
 end
