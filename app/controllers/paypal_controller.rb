585adb1cdb572d9f2d28e027e45e8e841c9b066c
Taku Matsumura <taku@MARINOS.(none)>
2013-03-24 21:43:25 +0900

    update

--- app/controllers/paypal_controller.rb
+++ app/controllers/paypal_controller.rb
@@ -6,7 +6,7 @@ class PaypalController < ApplicationController
   def notify
 
 ##development debug code
-params={"controller"=>"paypal", "action"=>"notify","txn_type"=>"subscr_signup", "subscr_id"=>"I-U3W9J558L1H0", "last_name"=>"\x8F\xBC\x91\xBA", "option_selection1"=>"", "residence_country"=>"JP", "mc_currency"=>"JPY", "item_name"=>"\x83`\x83F\x83b\x83N\x83p\x83X\x81@test", "business"=>"matsumuratk@yahoo.co.jp", "recurring"=>"1", "verify_sign"=>"ATasCS1Jl2VWHiyCH4RFZq70n7BIAS2u2BQTuSyUlmIOznUpJLEHcHDX", "payer_status"=>"unverified", "payer_email"=>"taku@matsmura.jp", "first_name"=>"\x91\xF1", "receiver_email"=>"matsumuratk@yahoo.co.jp", "payer_id"=>"RWTYZ5EDHXFJ6", "option_name1"=>"wpm1TP", "reattempt"=>"1", "item_number"=>"1234567", "subscr_date"=>"05:29:26 Mar 23, 2013 PDT", "btn_id"=>"59306892", "charset"=>"Shift_JIS", "notify_version"=>"3.7", "period1"=>"1 D", "mc_amount1"=>"0", "period3"=>"1 D", "mc_amount3"=>"1", "ipn_track_id"=>"73759ddd8db6e", "access_key"=>"wpm1TP"} if Rails.env.development?
+params={"controller"=>"paypal", "action"=>"notify","txn_type"=>"subscr_signup", "subscr_id"=>"I-U3W9J558L1H0", "last_name"=>"\x8F\xBC\x91\xBA", "option_selection1"=>"", "residence_country"=>"JP", "mc_currency"=>"JPY", "item_name"=>"\x83`\x83F\x83b\x83N\x83p\x83X\x81@test", "business"=>"matsumuratk@yahoo.co.jp", "recurring"=>"1", "verify_sign"=>"ATasCS1Jl2VWHiyCH4RFZq70n7BIAS2u2BQTuSyUlmIOznUpJLEHcHDX", "payer_status"=>"unverified", "payer_email"=>"taku@matsmura.jp", "first_name"=>"\x91\xF1", "receiver_email"=>"matsumuratk@yahoo.co.jp", "payer_id"=>"RWTYZ5EDHXFJ6", "option_name1"=>"wpm1TP", "reattempt"=>"1", "item_number"=>"1234567", "subscr_date"=>"05:29:26 Mar 23, 2013 PDT", "btn_id"=>"59306892", "charset"=>"Shift_JIS", "notify_version"=>"3.7", "period1"=>"1 D", "mc_amount1"=>"0", "period3"=>"1 D", "mc_amount3"=>"1", "ipn_track_id"=>"73759ddd8db6e", "access_key"=>"hBlVlR"} if Rails.env.development?
 
   begin
     #PayedLogセット
@@ -22,11 +22,15 @@ params={"controller"=>"paypal", "action"=>"notify","txn_type"=>"subscr_signup", 
     payedLog.ipn_track_id = params['ipn_track_id']
     payedLog.subscr_date = Time.parse(params['subscr_date']).in_time_zone("US/Pacific")
     payedLog.save!
+Rails.logger.info("payerLog.save")      
 
     #notify処理
-    checkin_item = CheckinItem.find_by_access_key(params[:access_key])
+    checkin_item = CheckinItem.find_by_access_key(params['access_key'])
+    raise "Checkin Item not found \'#{params['access_key']}\'" if checkin_item.nil?
+    
+Rails.logger.info("CheckinItem.find")      
 
-    case params[:txn_type]
+    case params['txn_type']
     when "subscr_signup"   
       #登録処理
       ## LEAVEの状態なら、更新しない
@@ -36,26 +40,30 @@ params={"controller"=>"paypal", "action"=>"notify","txn_type"=>"subscr_signup", 
         checkin_item.start_date = Date.today
         checkin_item.payment_date = payedLog.subscr_date
         checkin_item.limit_date = Date.today << 12
+Rails.logger.info("CheckinItem.save")      
         checkin_item.save!
+Rails.logger.info("CheckinItem.save")      
       end
     when "subscr_cancel"
       #キャンセル処理
       #statusをLEAVEに更新
       checkin_item.status = CheckinItem::LEAVE
       checkin_item.cancel_date = payedLog.subscr_date
+Rails.logger.info("CheckinItem.save")      
       checkin_item.save!
+Rails.logger.info("CheckinItem.save")      
     else
       #その他の処理がきた場合
-      Rails.logger.info("paypal#notify:txn_type=#{params[:txn_type]}")
-      Rails.logger.info(params)      
+      Rails.logger.error("paypal#notify:想定外の処理")
+      Rails.logger.error("paypal#notify:txn_type=#{params['txn_type']}")
+      Rails.logger.error(params)      
     end
 
-    respond_to do |format|
-      format.html { render :text => "OK" }
-    end
+    render :text => "OK" 
 
   rescue => e
     Rails.logger.error("paypal#notify error:#{e}")
+    render :text => "NG" 
   end
   end
 
